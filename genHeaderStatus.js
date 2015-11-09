8a751c42c80afa4a8e4764a24a6ce2c42f114ec8
DanLombardy <lombardy.dan@gmail.com>
2015-11-09 11:38:47 -0800

    add new version of genHeaderStatus that sets content heads for img, css, and js files. Also modified handleContent to run automatically and provide a 404.

--- genHeaderStatus.js
+++ genHeaderStatus.js
@@ -5,22 +5,29 @@ function genHeaderStatus(string, response){
   var resData = {};
 
   if (string.indexOf(".json") != -1){
-    response.setHeader("Content-Type", "application/json");
-    resData.status = 200;
-
-    return resData;
+    response.setHeader(200, {"Content-Type": "application/json"});
 
   }else if(string.indexOf(".html") != -1){
-    response.setHeader("Content-Type", "text/html");
-    resData.status = 200;
+    response.setHeader(200, {"Content-Type": "text/html"});
 
-    return resData;
+  }else if(string.indexOf(".png") != -1 || string.indexOf(".jpg") != -1 ||
+  string.indexOf(".gif") != -1 || string.indexOf(".svg") != -1){
+    response.setHeader(200, {"Content-Type" : image/string.slice(-3, str.length)});
+
+
+  }else if(string.indexOf(".jpeg") != -1){
+      response.setHeader(200, {"Content-Type": "image/jpeg"});
+
+  }else if(string.indexOf(".js") != -1){
+      response.setHeader(200, {"Content-Type": "application/javascript"});
+
+  }else if(string.indexOf(".css") != -1){
+        response.setHeader(200, {"Content-Type": "text/css"});
+
   }else{
-    response.setHeader("Content-Type", "text/plain");
-    resData.status = 200;
+      response.setHeader(200, {"Content-Type": "text/plain"});
 
-    return resData;
-  }
+    }
 };
 
 module.exports.genHeaderStatus = genHeaderStatus;
