6e0e4b9af8e5710aa33afaa08c5a332f9c17feef
DanLombardy <lombardy.dan@gmail.com>
2015-11-09 00:24:31 -0800

    Changed way of setting response status data

--- genHeaderStatus.js
+++ genHeaderStatus.js
@@ -5,18 +5,21 @@ function genHeaderStatus(string, response){
   var resData = {};
 
   if (string.indexOf(".json") != -1){
-    response.setHeader("Content-Type", "application/json");
+    response.writeHead(200,{"Content-Type": "application/json"});
+    resData.data = undefined;
     resData.status = 200;
 
     return resData;
 
   }else if(string.indexOf(".html") != -1){
-    response.setHeader("Content-Type", "text/html");
+    response.writeHead(200, {"Content-Type": "text/html"});
+    resData.data = undefined;
     resData.status = 200;
 
     return resData;
   }else{
-    response.setHeader("Content-Type", "text/plain");
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    resData.data = undefined;
     resData.status = 200;
 
     return resData;
