51bb888d9f10ccaad24c8ff2ed89090490a64b55
Jordan Bundy <jordan@thefoldagram.com>
2015-11-10 09:28:07 -0800

    Update example.js

--- example/example.js
+++ example/example.js
@@ -2,6 +2,10 @@ var butler = require('server-butler');
 var http = require('http');
 
 butler.set('/home', 'GET', 'Greetings!');
-http.createServer(function(req, res) { 
+var server = http.createServer(function(req, res) { 
   butler.retrieve(req, res); 
 });
+
+server.listen(3000, function() {
+  console.log('All systems operational');
+})
