aa3c2a40caeee8aa15cc06b3b4e16422e97c9da8
DanLombardy <lombardy.dan@gmail.com>
2015-11-09 16:32:10 -0800

    made genHeaderStatus.js simpler

--- genHeaderStatus.js
+++ genHeaderStatus.js
@@ -5,29 +5,30 @@ function genHeaderStatus(string, response){
   var resData = {};
 
   if (string.indexOf(".json") != -1){
-    response.setHeader(200, {"Content-Type": "application/json"});
+    resData.contentType = "application/json";
 
   }else if(string.indexOf(".html") != -1){
-    response.setHeader(200, {"Content-Type": "text/html"});
+    resData.contentType = "text/html";
 
   }else if(string.indexOf(".png") != -1 || string.indexOf(".jpg") != -1 ||
   string.indexOf(".gif") != -1 || string.indexOf(".svg") != -1){
-    response.setHeader(200, {"Content-Type" : image/string.slice(-3, str.length)});
-
+    resData.contentType = "image/" + string.slice(-3, str.length;
 
   }else if(string.indexOf(".jpeg") != -1){
-      response.setHeader(200, {"Content-Type": "image/jpeg"});
+      resData.contentType = "image/jpeg";
+
 
   }else if(string.indexOf(".js") != -1){
-      response.setHeader(200, {"Content-Type": "application/javascript"});
+      resData.contentType = "application/javascript";
 
   }else if(string.indexOf(".css") != -1){
-        response.setHeader(200, {"Content-Type": "text/css"});
+        resData.contentType = "text/css";
 
   }else{
-      response.setHeader(200, {"Content-Type": "text/plain"});
+      resData.contentType = "text/plain";
 
-    }
+  }
+  response.setHeader(200, {"Content-Type": resData.contentType});
 };
 
 module.exports.genHeaderStatus = genHeaderStatus;
